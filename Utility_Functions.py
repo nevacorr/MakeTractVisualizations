589df643cd75e309c4809ad1522551a554bc8453
Neva Corrigan <nevao@uw.edu>
2025-03-24 18:01:37 -0700

    Create images for all tracts

--- Utility_Functions.py
+++ Utility_Functions.py
@@ -1,11 +1,35 @@
 
 import pandas as pd
 import numpy as np
+from fury import actor
 
-def load_data(filepath, filename):
+def load_data(filepath, filename, tract):
     df = pd.read_csv(f"{filepath}{filename}")
 
-    z = df.loc[df["Tract"] == "Left.Arcuate", "Z_mean"].values
-    adjp = df.loc[df["Tract"] == "Left.Arcuate", "adjusted_p_value"].values
+    tract_mapping = {'Left.Arcuate': 'ARC_L', 'Right.Arcuate':'ARC_R', 'Left.Thalamic.Radiation':'ATR_L',
+                       'Right.Thalamic.Radiation':'ATR_R', 'Left.IFOF':'IFO_L', 'Right.IFOF':'IFO_R', 'Left.ILF':'ILF_L',
+                       'Right.ILF':'ILF_R', 'Left.SLF':'SLF_L', 'Right.SLF':'SLF_R', 'Left.Uncinate':'UNC_L',
+                       'Right.Uncinate':'UNC_R', 'Left.Corticospinal':'CST_L', 'Right.Corticospinal':'CST_R'}
 
+    df['Tract'] = df['Tract'].replace(tract_mapping)
+
+    z = df.loc[df["Tract"] == tract, "Z_mean"].values
+    adjp = df.loc[df["Tract"] == tract, "adjusted_p_value"].values
     return z, adjp
+
+def lines_as_tubes(sl, line_width, **kwargs):
+    line_actor = actor.line(sl, **kwargs)
+    line_actor.GetProperty().SetRenderLinesAsTubes(1)
+    line_actor.GetProperty().SetLineWidth(line_width)
+    return line_actor
+
+def trim_to_central_60(streamlines):
+    trimmed_streamlines = []
+    for streamline in streamlines:
+        # Calculate the number of points to keep (60% of the streamline)
+        num_points = len(streamline)
+        start_index = int(num_points * 0.2)  # 20% from the start
+        end_index = int(num_points * 0.8)    # 20% from the end
+        trimmed_streamline = streamline[start_index:end_index]
+        trimmed_streamlines.append(trimmed_streamline)
+    return trimmed_streamlines

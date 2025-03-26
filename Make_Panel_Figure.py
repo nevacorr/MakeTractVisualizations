71b2c928eb38cfc0da8ce156615de0f0c669ce6d
Neva Corrigan <nevao@uw.edu>
2025-03-26 15:34:57 -0700

    loop through all tracts

--- Make_Panel_Figure.py
+++ Make_Panel_Figure.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import os.path as op
+import os
 
-# List of image file paths
-image_files = ['F_ARC_L.png', 'F_ARC_R.png', 'M_ARC_L.png', 'M_ARC_R.png']
+metric='md'
 
 # Define crop margins (top, bottom, left, right)
 crop_top = 200
@@ -10,19 +11,35 @@ crop_bottom = 200
 crop_left = 200
 crop_right = 200
 
-# Create figure
-fig, axes = plt.subplots(2, 2, figsize=(8, 8))
+working_dir = os.getcwd()
 
-# Flatten axes to a 1D list
-axes = axes.ravel()
+tract_ids = ['ARC', 'ATR', 'IFO', 'ILF', 'SLF', 'UNC', 'CST']
 
-# Loop through images and plot them
-for ax, img_path in zip(axes, image_files):
-    img = mpimg.imread(img_path)  # Read image
-    h, w, _ = img.shape  # Get image dimensions
-    img_cropped = img[crop_top:h-crop_bottom, crop_left:w-crop_right]  # Crop with fixed margins
-    ax.imshow(img_cropped)  # Display cropped image
-    ax.axis("off")  # Hide axes
+for tid in tract_ids:
 
-plt.tight_layout()
-plt.show()
+    image_files = [f'{metric}_F_{tid}_L.png', f'{metric}_F_{tid}_R.png', f'{metric}_M_{tid}_L.png', f'{metric}_M_{tid}_R.png']
+
+    # Create figure
+    fig, axes = plt.subplots(2, 2, figsize=(8, 8))
+
+    # Flatten axes to a 1D list
+    axes = axes.ravel()
+
+    # Loop through images and plot them
+    for ax, img_path in zip(axes, image_files):
+        img = mpimg.imread(img_path)  # Read image
+        h, w, _ = img.shape  # Get image dimensions
+        img_cropped = img[crop_top:h-crop_bottom, crop_left:w-crop_right]  # Crop with fixed margins
+        ax.imshow(img_cropped)  # Display cropped image
+        ax.axis("off")  # Hide axes
+
+    # Add text to the figure
+    fig.text(0.5, 0.95, f"Female {metric.upper()} {tid}", fontsize=20, ha='center', va='center', color='black')
+    fig.text(0.5, 0.45, f"Male {metric.upper()} {tid}", fontsize=20, ha='center', va='center', color='black')
+
+    plt.tight_layout()
+    plt.savefig(op.join(working_dir, f'figure_{metric}_{tid}.png'))
+    plt.show()
+
+
+

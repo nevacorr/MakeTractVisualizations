ba6ab193b7a283ae75a1362c0e80e7c63c511788
Neva Corrigan <nevao@uw.edu>
2025-04-01 16:27:59 -0700

    this code correctly aligns streamlines and brain!!!!!

--- Inspect_AFQ_mat_files.py
+++ Inspect_AFQ_mat_files.py
@@ -1,4 +1,3 @@
-import h5py
 import os.path as path
 import scipy.io
 import nibabel as nib
@@ -9,30 +8,24 @@ mat_dir = '/Users/neva/PycharmProjects/VisualizeTracts/genz323/dti64trilin'
 mat_file = 'fiberfiles_genz323.mat' # This was a file that I manually exported from matlab after loading the
                                     # AFQ output .mat file given to me for this subject MoriGroups_Cortex_clean_D5_L4.mat).
                                     # The original .mat file contained other variables and was in HDF5 format
+dti_img_file = 'bin/mpfcoreg12.nii.gz'
 
-
+# Load data
 mat_data = scipy.io.loadmat(path.join(mat_dir, mat_file))
+dti_space_img = nib.load(path.join(mat_dir, dti_img_file))
+
+# Extract image properties
+affine = dti_space_img.affine  # Get the affine transformation
+dims = dti_space_img.shape  # Get the voxel dimensions
 
 fg = mat_data['fg']
-
-# Print the type and contents of 'fg'
-print(type(fg))
-# If 'fg' is a structured array, you can list its fields
-if fg.dtype.names:
-    print(fg.dtype.names)
-
 fibers = fg['fibers']
-
-names = fg['name'] # tract region name
-
-# Create an identity matrix affine (no transformation)
-# Would be better to include a real affine
-identity_affine = np.eye(4)
+names = fg['name'] # tract region names
 
 for i in range(fibers.shape[1]):  # Loop over the 20 regions
     current_fibers = fibers[0, i]  # Extract the current region's fibers
-
     fiber_data = []
+
     for j in range(current_fibers.shape[0]):  # Loop over each fiber (streamline)
         fiber = current_fibers[j]  # Extract the current fiber
 
@@ -60,9 +53,9 @@ for i in range(fibers.shape[1]):  # Loop over the 20 regions
     trk_filename = trk_filename.replace("'", "")
 
     # Create Tractogram object
-    tractogram = nib.streamlines.Tractogram(streamline_sequence, affine_to_rasmm=identity_affine)
+    tractogram = nib.streamlines.Tractogram(streamline_sequence, affine_to_rasmm=affine)
 
     # Save to .trk file
-    nib.streamlines.save(tractogram, trk_filename)
+    nib.streamlines.save(tractogram, path.join('new_trk_files', trk_filename))
     print(f"Saved fibers for {row_name} to {trk_filename}")
     mystop=1

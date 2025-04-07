35966af6546b7128d9b10f6d13a6a83d8ec58aef
nevacorr <nevao@uw.edu>
2025-04-07 14:54:36 -0700

    Make panel figures for streamlines derived from genz data

--- Make_Panel_Figure.py
+++ Make_Panel_Figure.py
@@ -3,7 +3,7 @@ import matplotlib.image as mpimg
 import os.path as op
 import os
 
-metric='mpf'
+metric='md'
 
 # Define crop margins (top, bottom, left, right)
 crop_top = 200
@@ -12,23 +12,26 @@ crop_left = 200
 crop_right = 200
 
 working_dir = os.getcwd()
-profdir = os.path.join(working_dir, 'R_tract_profiles')
+profdir = os.path.join(working_dir, 'tract_profiles_from_R')
+streamline_dir = os.path.join(working_dir, 'individual_modality_figs')
 
-tract_ids = ['ARC', 'ATR', 'IFO', 'ILF', 'SLF', 'UNC', 'CST']
+tract_ids = ['Arcuate', 'Thalamic_Radiation', 'IFOF', 'ILF', 'SLF', 'Uncinate', 'Corticospinal']
+# tract_ids = ['ARC', 'ATR', 'IFO', 'ILF', 'SLF', 'UNC', 'CST']
 profiles_regions = ['Arcuate', 'Thalamic.Radiation', 'IFOF', 'ILF', 'SLF', 'Uncinate', 'Corticospinal']
 
 # Create a mapping from tract_ids to profile_regions
 tract_to_profile = dict(zip(tract_ids, profiles_regions))
 
 if metric == 'mpf':
-    tract_ids.remove('UNC')
-    del tract_to_profile['UNC']
+    tract_ids.remove('Uncinate')
+    del tract_to_profile['Uncinate']
 
 for tid in tract_ids:
     print(f'{tid}')
     p = tract_to_profile[tid]
-    image_files = [f'{metric}_F_{tid}_L.png', f'{metric}_F_{tid}_R.png', f'{metric}_M_{tid}_L.png',
-                   f'{metric}_M_{tid}_R.png', f'{profdir}/tracts_{metric}_splits_100Left.{p}.png',
+    image_files = [f'{streamline_dir}/{metric}_F_Left_{tid}.png', f'{streamline_dir}/{metric}_F_Right_{tid}.png',
+                   f'{streamline_dir}/{metric}_M_Left_{tid}.png',
+                   f'{streamline_dir}/{metric}_M_Right_{tid}.png', f'{profdir}/tracts_{metric}_splits_100Left.{p}.png',
                    f'{profdir}/tracts_{metric}_splits_100Right.{p}.png']
 
     # Create figure
@@ -58,6 +61,7 @@ for tid in tract_ids:
     fig.text(0.95, 0.65, "R", fontsize=15, ha='center', va='center', color='black')
 
     plt.tight_layout()
+    os.makedirs(op.join(working_dir, 'panel_figures'), exist_ok=True)
     plt.savefig(op.join(working_dir, 'panel_figures', f'figure_{metric}_{tid}.png'))
     plt.show()
 
